ea684de9f0fda559ad3192aea1dca9b129f5ad05
Marian4gc <mariangarcia1724@gmail.com>
2023-04-19 20:48:02 +0200

    mejoras

--- Front/src/components/results/AllBirds.jsx
+++ Front/src/components/results/AllBirds.jsx
@@ -11,27 +11,65 @@ function AllBirds() {
             .then(data => setData(data));
     }, []);
 
+    const aves = {
+        "Abejaruco europeo": "Abejaruco europeo",
+        "Abubilla común": "Abubilla común",
+        "Ánsar común": "Ánsar común",
+        "Autillo europeo": "Autillo europeo",
+        "Avión común": "Avión común",
+        "Cigüeña blanca": "Cigüeña blanca",
+        "Codorniz común": "Codorniz común",
+        "Cuco común": "Cuco común",
+        "Golondrina común": "Golondrina común",
+        "Grulla común": "Grulla común",
+        "Pardela cenicienta": "Pardela cenicienta",
+        "Ruiseñor común": "Ruiseñor común",
+        "Tortola europea": "Tortola europea",
+        "Vencejo común": "Vencejo común",
+    };
+
+    const plantas = {
+        "almendro": "Almendro",
+        "Amapola": "Amapola",
+        "Castaño": "Castaño",
+        "Jara Pringosa": "Jara Pringosa",
+        "Plátano de paseo": "Plátano de paseo",
+        // Agrega más plantas aquí
+    };
+
+    const insectos = {
+        "abeja": "Abeja",
+        "Chupaleche": "Chupaleche",
+        // Agrega más insectos aquí
+    };
+
     const filteredData = data.filter(item => {
-        return item.name !== "" && item.user !== "";
+        return item.name !== "";
     });
 
     return (
         <Table striped bordered hover>
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>User</th>
+                    <th>Aves</th>
+                    <th>Plantas</th>
+                    <th>Insectos</th>
                 </tr>
             </thead>
             <tbody>
-                {filteredData.map((item, index) => (
-                    <tr key={index}>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.user}</td>
-                    </tr>
-                ))}
+                {filteredData.map((item, index) => {
+                    let ave = aves[item.name] || "-";
+                    let planta = plantas[item.name] || "-";
+                    let insecto = insectos[item.name] || "-";
+                    return (
+                        <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>{ave !== "" ? ave : "-"}</td>
+                            <td>{planta !== "" ? planta : "-"}</td>
+                            <td>{insecto !== "" ? insecto : "-"}</td>
+                        </tr>
+                    );
+                })}
             </tbody>
         </Table>
     );
