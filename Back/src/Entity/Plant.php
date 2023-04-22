2af46f0226f9c0b939c326c4bd6601d49e72f69d
Marian4gc <mariangarcia1724@gmail.com>
2023-04-22 17:28:38 +0200

    boton de volver del back e imagen back

--- Back/src/Entity/Plant.php
+++ Back/src/Entity/Plant.php
@@ -18,7 +18,7 @@ class Plant
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 500)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
